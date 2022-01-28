243f3b22991ffcf484a9814538e03c3943b110f5
javimaster2 <jarquinhector40@gmail.com>
2022-01-27 18:04:02 -0600

    engennosv6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,12 +25,7 @@
         
        
         
-        <link
-        rel="stylesheet"
-        href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
-      />
-      
-      <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+ 
        {{--  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"> --}}
     {{-- <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
